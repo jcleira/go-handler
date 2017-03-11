a32d5bc5a54f1c2b71ef6e18647a3211a9aa1f14
jcorral <jmc.leira@gmail.com>
2017-03-11 19:00:47 +0100

    Add a HTTPError class to manage StatusCode returns & log errors

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,24 @@
 package handler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
-type CustomHandler func(http.ResponseWriter, *http.Request) error
+type CustomHandler func(http.ResponseWriter, *http.Request) *HTTPError
+
+// HTTPError struct implements the Error interface to make easier return HTTP
+// errors. Using this HTTPError we are able to return the error plus the HTTP
+// Status Code that we want the middleware to return on the request.
+type HTTPError struct {
+	Err        error `json:"error"`
+	StatusCode int   `json:"status_code"`
+}
+
+// Error interface implementation.
+func (ce *HTTPError) Error() string {
+	return ce.Err.Error()
+}
 
 // ServerHTTP is our custom implementation needed to satisfy the Handler
 // interface.
@@ -10,7 +26,8 @@ func (ch CustomHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	if err := ch(w, req); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		log.Println(err)
+
+		http.Error(w, err.Error(), err.StatusCode)
 	}
 }
