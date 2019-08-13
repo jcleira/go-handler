34e9919c5a38428c7e8f7e9bf9e775180c0de502
jcleira <jmc.leira@gmail.com>
2019-08-13 17:13:46 +0200

    Remove our custom error replaced by google/jsonapi ones
    This commit removes the jsonapi errors definition that we initially
    created, and replaced them with the google/jsonapi error definition.
    
    It does also changes the way that we invoke the Handler from the users
    perspective

--- handler.go
+++ handler.go
@@ -4,26 +4,25 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/google/jsonapi"
 )
 
-// Handler it our Handler func enhanced to early return errors.
-type Handler func(http.ResponseWriter, *http.Request) *Errors
+// Handler is our custom Handler func enhanced to do early return for errors.
+type Handler func(http.ResponseWriter, *http.Request) *jsonapi.ErrorsPayload
 
-// WithLog is the primary custom middleware, it has logging responsibility for
-// request's errors, but also converts the Handler to a http.Handler.
-//
-// Returns a http.Handler ready to be added to the touter.
-func (fn Handler) WithLog() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+// ServeHTTP func that will make our custom Handler compliant with the
+// http.Handler interface.
+func (fn Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-		if e := fn(w, r); e != nil {
-			defer log.Println(e.Error())
-			w.WriteHeader(e.StatusCode)
-
-			if err := json.NewEncoder(w).Encode(e); err != nil {
-				defer log.Println("Unable to write the JSON error response")
-			}
+	if errors := fn(w, r); errors != nil {
+		for i := 0; i < len(errors.Errors); i++ {
+			log.Println(errors.Errors[i].Error())
 		}
-	})
+
+		if err := json.NewEncoder(w).Encode(errors); err != nil {
+			defer log.Println("Unable to write the JSON error response")
+		}
+	}
 }
